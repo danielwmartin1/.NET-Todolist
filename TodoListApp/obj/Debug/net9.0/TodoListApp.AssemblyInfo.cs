a78ff186a18e6e57da8da1fba4ebe88543907318
Daniel Martin <danielwmartin1@gmail.com>
2025-03-05 02:29:13 -0500

    Add Supabase package and update appsettings for integration

--- TodoListApp/obj/Debug/net9.0/TodoListApp.AssemblyInfo.cs
+++ TodoListApp/obj/Debug/net9.0/TodoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1c4df53775cc9243d97694bc92eb2ba3e2045b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fbf6a527d309b238fa5d54076ae8000686d27d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
