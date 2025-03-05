8c1fc8a8cf56c2b74b3da1f11136a2b0b05e6ce2
Daniel Martin <danielwmartin1@gmail.com>
2025-03-05 03:08:03 -0500

    Update binaries and assembly information for Todo List application

--- TodoListApp/obj/Debug/net9.0/TodoListApp.AssemblyInfo.cs
+++ TodoListApp/obj/Debug/net9.0/TodoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fbf6a527d309b238fa5d54076ae8000686d27d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e8da405022c1025971aa83c9bf36c4071c49fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
