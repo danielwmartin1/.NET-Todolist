a61b864cd708ee49a0e13b3954985b684274ab9c
Daniel Martin <danielwmartin1@gmail.com>
2025-03-08 10:13:26 -0500

    Update binaries and refresh Last-Modified timestamps in Todo List application

--- TodoListApp/obj/Debug/net9.0/TodoListApp.AssemblyInfo.cs
+++ TodoListApp/obj/Debug/net9.0/TodoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d2f8b6d4ba6848057129c9e1d73e5c8dd6555c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f2bbbb0199fb42757ac5d65eb26c6c15c5a56b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
