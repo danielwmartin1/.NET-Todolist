f1e3130447e987881c7d4cf25acb6d4c6aa7ebbe
Daniel Martin <danielwmartin1@gmail.com>
2025-03-27 01:57:33 -0400

    Update binaries, enhance button spacing, and improve edit form functionality in TodoListApp

--- TodoListApp/obj/Debug/net9.0/TodoListApp.AssemblyInfo.cs
+++ TodoListApp/obj/Debug/net9.0/TodoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b16e5e2a7a31ddf0cbb10569f598d1d2699e99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f77a95e4907457822efd958de64ad05eb3f3546")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
