48987d0040d3a52b790c8557259d27df2f1b9d66
Daniel Martin <danielwmartin1@gmail.com>
2025-03-31 15:06:33 -0400

    Update configuration in TodoListApp

--- TodoListApp/obj/Debug/net9.0/TodoListApp.AssemblyInfo.cs
+++ TodoListApp/obj/Debug/net9.0/TodoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f03e3071003f010feaba6741f87871526c95eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dd74f06e3a21a0cda0dd270dcfc11941aaf723d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
